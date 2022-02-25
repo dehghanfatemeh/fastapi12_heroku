2c390712d2946c362f80525c9a7e43c17adadd63
Fatemeh Dehghan <fdghn77@gmail.com>
2022-02-25 17:18:53 +0330

    change3 file main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app=FastAPI()
 
 Students={
     'ali':[12,17.7,18],
-    'sara':[14.7,18,15.5],
+    'sara':[14.25,18,15.75],
     'hasan':[10,14.75,13.56],
     'reza':[11.5,16.5,13.25]
 }
@@ -20,11 +20,8 @@ def read():
 @app.post('/average')
 def average():
     a=[]
-    s=0
     for i in df.columns:
-        for j in df.index:
-            s=df.loc[j,i]+s
-        s=s/len(df.index)
+        s=df[i].mean()
         a.append(s)
         s=0
     df.loc['ave']=a
@@ -34,7 +31,21 @@ def average():
 @app.put('/update/{name}')
 def update(name:str):
     if name in df.columns:
-        df[name]=df[name].apply(lambda y: y+1)
+        for i in df.columns:
+            for j in df.index:
+                if j!='ave':
+                    df.loc[j,i]=df.loc[j,i]+1
+
+        s=m=0
+        for n in df[name]:
+                if m!=3:
+                    s+=n
+                    m+=1
+        df.loc['ave',name]=s/(len(df.index)-1)
+
+    # df[name]=df[name].apply(lambda y: y+1)
+       
+       
         df_json=df.to_json()
         return json.loads(df_json)
     else:
@@ -43,7 +54,7 @@ def update(name:str):
 @app.delete('/delete/{name}')
 def delete(name:str):
     if name in df.columns:
-        df.drop(columns=name,axis=0,inplace=True)
+        df.drop(columns=name,axis=1,inplace=True)
         df_json=df.to_json()
         return json.loads(df_json)
     else:
