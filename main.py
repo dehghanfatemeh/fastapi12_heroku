c40448348cd2caf0c8344afc282f02c483dcd336
Fatemeh Dehghan <fdghn77@gmail.com>
2022-02-25 13:40:46 +0330

    change1 main

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 import json
 import pandas as pd
 app=FastAPI()
 
-students={
+Students={
     'ali':[12,17.7,18],
-    'sara':[14.7,18,15.25],
-    'hasan':[10,20,13.56]
+    'sara':[14.7,18,15.5],
+    'hasan':[10,14.75,13.56],
+    'reza':[11.5,16.5,13.25]
 }
 
-df=pd.DataFrame(students,columns=students.keys())
-
-df_json=df.to_json()
+df=pd.DataFrame(Students,columns=Students.keys())
 
 @app.get('/')
 def read():
+    df_json=df.to_json()
     return json.loads(df_json)
 
-
-@app.post('/means')
-def means():
+@app.post('/average')
+def average():
     a=[]
     s=0
     for i in df.columns:
@@ -30,19 +29,22 @@ def means():
         s=0
     df.loc['ave']=a
     df_json=df.to_json()
-
     return json.loads(df_json)
 
+@app.put('/update/{name}')
+def update(name:str):
+    if name in df.columns:
+        df[name]=df[name].apply(lambda y: y+1)
+        df_json=df.to_json()
+        return json.loads(df_json)
+    else:
+        raise HTTPException(status_code=404,detail='This student is not available')
 
-@app.post('/shower/{t}')
-def shower(t:str):
-    d=df[t]
-    d_json=d.to_json()
-    return json.loads(d_json)
-
-
-@app.put('/update/{st}')
-def update(st:str):
-    df[st]=df[st].apply(lambda y: y+1 )
-    df_json=df.to_json()
-    return json.loads(df_json)
+@app.delete('/delete/{name}')
+def delete(name:str):
+    if name in df.columns:
+        df.drop(columns=name,axis=0,inplace=True)
+        df_json=df.to_json()
+        return json.loads(df_json)
+    else:
+        raise HTTPException(status_code=404,detail='This student is not available')
